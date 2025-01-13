357bd429ab901ea887bb8e40ddcc6fa7c3e220d9
Ebite God'sgift Uloamaka <godsgiftuloamaka235@gmail.com>
2025-01-14 00:38:53 +0100

    server set up

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/uloamaka/rss_aggregator
+
+go 1.21.5
+
+require (
+	github.com/go-chi/chi/v5 v5.2.0
+	github.com/joho/godotenv v1.5.1
+)
+
+require github.com/go-chi/cors v1.2.1
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	godotenv.Load(".env")
+
+	
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300,
+	}))
+
+	srv := &http.Server {
+		Handler: router,
+		Addr: ":" + portString,
+	}
+
+	fmt.Println("Server running on port", portString)
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
